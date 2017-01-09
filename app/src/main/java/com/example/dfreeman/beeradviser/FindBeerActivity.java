e231ebf64fe0ba61967b1896995a4095ce9b7b6a
dfreemanRIIS <dfreeman@riis.com>
2017-01-09 13:55:33 -0500

    Connect activity

--- app/src/main/java/com/example/dfreeman/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/example/dfreeman/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.example.dfreeman.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner)findViewById(R.id.color);
+
+        //Get selects color
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Display results
+        brands.setText(beerType);
+
     }
 }
